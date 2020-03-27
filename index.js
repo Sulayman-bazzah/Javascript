8de4a784c6d0f6d4b705051b06d67f0ca4368850
sulayman_bazzah <sulaymanbazzah.sb@gmail.com>
2020-03-27 22:17:46 +0100

    javascript task 1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-<script>
+
     var name = sulaiman;
     var courses = ['HTML', 'CSS', 'JAVASCRIPT'];
     console.log(name);
@@ -8,7 +8,6 @@
     for(courses = 1; courses <= 200; courses++)
     {
         if (courses % 2!== 0){
-            console.log(courses)
+            console.log(courses);
         }
     }
-</script>
